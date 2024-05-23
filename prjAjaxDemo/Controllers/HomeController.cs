d3d9740c2805197d2f0227613942c5138d7eac9d
Julie <juliachiu777@gmail.com>
2024-05-23 15:46:11 +0800

    加入專案檔案。

---
+++ prjAjaxDemo/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjAjaxDemo.Models;
+using System.Composition.Hosting.Core;
+using System.Diagnostics;
+
+namespace prjAjaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly MyDBContext _context;
+        public HomeController(ILogger<HomeController> logger, MyDBContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View(_context.Categories.ToList());
+        }
+
+        public IActionResult CallAPI()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult JSONTest()
+        {
+            return View();
+        }
+
+        public IActionResult first()
+        {
+            return View();
+        }
+
+        public IActionResult Address()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Spots()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ prjAjaxDemo/Controllers/HomeWorkController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjAjaxDemo.Controllers
+{
+    public class HomeWorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HomeWork()
+        {
+            return View();
+        }
+    }
+}
